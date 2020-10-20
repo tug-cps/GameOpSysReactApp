dc6dfd83d3b6adae006654d63399c66155149888
ThomasSchwengler <ThomasSchwengler@users.noreply.github.com>
2020-10-20 09:29:50 +0000

    deploy: 4925f105aa78c80689a61189793f3ed1b697968b

---
+++ precache-manifest.d0d851eb8cf37b15e20bd8b12a1a947b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb33a25b7224bff2dff5cea034f7ee4",
+    "url": "/GameOpSysReactApp/index.html"
+  },
+  {
+    "revision": "030094018e115661a15b",
+    "url": "/GameOpSysReactApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a4771d65f611c39f883",
+    "url": "/GameOpSysReactApp/static/js/2.a018e603.chunk.js"
+  },
+  {
+    "revision": "c2472c16fee4ab69feb7db794e2beb30",
+    "url": "/GameOpSysReactApp/static/js/2.a018e603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "030094018e115661a15b",
+    "url": "/GameOpSysReactApp/static/js/main.8f32efd1.chunk.js"
+  },
+  {
+    "revision": "5e5bc1849afe32fe17ac",
+    "url": "/GameOpSysReactApp/static/js/runtime-main.faf33a2b.js"
+  }
+]);
