430e7ec4c7327e2d40e48ab2025a437cd6461b1c
Thomas Schwengler <thomas.schwengler@student.tugraz.at>
2021-09-07 15:22:08 +0200

    Issue-62: Create basic mood page

--- src/Home.tsx
+++ src/Home.tsx
@@ -15,10 +15,9 @@ import {
     WithStyles
 } from "@material-ui/core";
 import {Link as RouterLink} from "react-router-dom";
-import {WithTranslation, withTranslation} from "react-i18next";
+import {useTranslation, WithTranslation, withTranslation} from "react-i18next";
 import DefaultAppBar from "./common/DefaultAppBar";
-import ShowChartIcon from '@material-ui/icons/ShowChart';
-import HistoryIcon from '@material-ui/icons/History';
+import {homeDestinations} from "./common/Destinations";
 
 const styles = ({palette}: Theme) => createStyles({
     media: {
@@ -36,7 +35,7 @@ interface Item {
     title: string;
     subtitle: string;
     icon: any;
-    destination: string;
+    to: string;
     header: boolean
 }
 
@@ -45,28 +44,27 @@ interface CardProps {
 }
 
 const HomeCard = withStyles(styles)(
-    class extends React.Component<CardProps & WithStyles<typeof styles>, {}> {
-        render() {
-            const {classes, item} = this.props
+    function Component(props: CardProps & WithStyles<typeof styles>) {
+        const {classes, item} = props
+        const {t} = useTranslation()
 
-            return (
-                <Grid item xs={12} sm={item.header ? 12 : 6} xl={item.header ? 12 : 4} key={item.title}>
-                    <Card>
-                        <CardActionArea component={RouterLink} to={item.destination}>
-                            <Box display="flex">
-                                <CardMedia className={classes.media}>
-                                    <SvgIcon component={item.icon} className={classes.icon}/>
-                                </CardMedia>
-                                <CardContent>
-                                    <Typography variant="h6">{item.title}</Typography>
-                                    <Typography color="textSecondary" noWrap>{item.subtitle}</Typography>
-                                </CardContent>
-                            </Box>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-            )
-        }
+        return (
+            <Grid item xs={12} sm={item.header ? 12 : 6} xl={item.header ? 12 : 4} key={item.title}>
+                <Card>
+                    <CardActionArea component={RouterLink} to={item.to}>
+                        <Box display="flex">
+                            <CardMedia className={classes.media}>
+                                <SvgIcon component={item.icon} className={classes.icon}/>
+                            </CardMedia>
+                            <CardContent>
+                                <Typography variant="h6">{t(item.title)}</Typography>
+                                <Typography color="textSecondary" noWrap>{t(item.subtitle)}</Typography>
+                            </CardContent>
+                        </Box>
+                    </CardActionArea>
+                </Card>
+            </Grid>
+        )
     }
 )
 
@@ -74,31 +72,13 @@ class Home extends React.Component<WithTranslation> {
 
     render() {
         const {t} = this.props;
-
-        const items: Item[] = [
-            {
-                title: t('card_power_title'),
-                subtitle: t('card_power_subtitle'),
-                icon: ShowChartIcon,
-                destination: '/power',
-                header: false
-            },
-            {
-                title: t('card_archive_title'),
-                subtitle: t('card_archive_subtitle'),
-                icon: HistoryIcon,
-                destination: '/archive',
-                header: false
-            },
-        ]
-
         return (
             <React.Fragment>
                 <DefaultAppBar hideBackButton title={t('home_title')}/>
                 <Container maxWidth="lg" disableGutters>
                     <Box padding={1}>
                         <Grid container>
-                            {items.map((item: Item, index: number) => <HomeCard item={item} key={index}/>)}
+                            {homeDestinations.map((item: Item, index: number) => <HomeCard item={item} key={index}/>)}
                         </Grid>
                     </Box>
                 </Container>
