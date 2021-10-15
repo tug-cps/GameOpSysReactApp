79583c22af959d252bcd7e440b7208dd9a244e6c
Thomas Schwengler <thomas.schwengler@student.tugraz.at>
2021-10-15 09:18:27 +0200

    Fix: Update app bar color to a more modern look

--- src/common/ResponsiveIconButton.tsx
+++ src/common/ResponsiveIconButton.tsx
@@ -8,21 +8,36 @@ interface Props {
     requiresAttention?: boolean
 }
 
+function AttentionIcon(props: { requiresAttention?: boolean, icon: JSX.Element }) {
+    return <Badge invisible={!props.requiresAttention} variant="dot" color="info">{props.icon}</Badge>
+}
+
+function SmallIconButton(props: Props) {
+    return (
+        <Tooltip title={props.description}>
+            <IconButton
+                color="inherit"
+                onClick={props.onClick}
+                size="large"
+                children={<AttentionIcon {...props}/>}
+            />
+        </Tooltip>
+    )
+}
+
+function LargeIconButton(props: Props) {
+    return (
+        <Button startIcon={<AttentionIcon {...props}/>}
+                color="inherit"
+                onClick={props.onClick}
+                children={props.description}
+        />
+    )
+}
+
 export function ResponsiveIconButton(props: Props) {
     const theme = useTheme();
     const breakpoint = useMediaQuery(theme.breakpoints.down('sm'));
-
-    const AttentionIcon = () => <Badge invisible={!props.requiresAttention}
-                                       variant="dot"
-                                       color="info">{props.icon}</Badge>
-    return <>
-        {breakpoint &&
-        <Tooltip title={props.description}>
-            <IconButton color="inherit" onClick={props.onClick} size="large"><AttentionIcon/></IconButton>
-        </Tooltip>
-        }
-        {!breakpoint &&
-        <Button startIcon={<AttentionIcon/>} color="inherit" onClick={props.onClick}>{props.description}</Button>
-        }
-    </>;
+    if (breakpoint) return <SmallIconButton {...props}/>
+    return <LargeIconButton {...props}/>
 }
