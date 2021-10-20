5aaaf77c24107d4a8840d0d77b19ba8b81cbda5a
Thomas Schwengler <thomas.schwengler@student.tugraz.at>
2021-10-20 08:42:55 +0200

    Fix: Styling of feedback charts

--- src/Feedback.tsx
+++ src/Feedback.tsx
@@ -1,10 +1,10 @@
 import InfoOutlined from "@mui/icons-material/InfoOutlined";
-import {Container, Grid, Paper} from "@mui/material";
+import {Container, Grid, Paper, useTheme} from "@mui/material";
 import {blue, green, red, yellow} from "@mui/material/colors";
-import {Chart, ChartData, ChartOptions, LegendItem} from "chart.js";
+import {ChartData, ChartOptions} from "chart.js";
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import {isPast, isValid} from "date-fns";
-import React, {useEffect} from 'react';
+import React, {useEffect, useMemo} from 'react';
 import {Bar, Pie} from "react-chartjs-2";
 import {useTranslation} from "react-i18next";
 import {Redirect, useLocation} from "react-router-dom";
@@ -14,81 +14,108 @@ import {InfoDialog, Lorem, useInfoDialog} from "./common/InfoDialog";
 import ResponsiveIconButton from "./common/ResponsiveIconButton";
 import useDefaultTracking from "./common/Tracking";
 
-const barChartData: ChartData = {
-    labels: ['Ihr Stromverbrauch', 'Durchschnittlicher Stromverbrauch der anderen'],
-    datasets: [
-        {
-            data: [15, 10],
-            backgroundColor: [red["500"], blue["500"]],
-            borderColor: "#efefef",
-            borderWidth: 2,
-            barPercentage: 0.9,
-            categoryPercentage: 0.9
-        },
-    ],
+const useBarChartData: () => ChartData = () => {
+    const theme = useTheme();
+    return useMemo(() => ({
+        labels: ['Stromverbrauch'],
+        datasets: [
+            {
+                label: 'Ihr Stromverbrauch',
+                data: [4],
+                backgroundColor: red["800"],
+                borderColor: theme.palette.background.paper,
+            },
+            {
+                label: 'Durchschnittlicher Stromverbrauch der anderen',
+                data: [3.6],
+                backgroundColor: blue["800"],
+                borderColor: theme.palette.background.paper,
+            },
+        ],
+    }), [theme]);
 };
-
-const pieChartData: ChartData = {
-    labels: ['Viel Strom', 'Durchschnittlicher Strom', 'Wenig Strom'],
-    datasets: [
-        {
-            data: [40, 35, 25],
-            backgroundColor: [green["500"], yellow["500"], red["500"]],
-            barPercentage: 0.9,
-            categoryPercentage: 0.9
-        },
-    ],
-};
-
-
-const options: ChartOptions = {
-    scales: {
-        xAxis: {
-            ticks: {display: false},
-            grid: {display: false}
-        },
-        yAxis: {display: false}
-    },
-    plugins: {
-        legend: {
-            display: true,
-            position: 'bottom',
-            labels: {
-                generateLabels(chart: Chart): LegendItem[] {
-                    const colors = chart.data.datasets[0].backgroundColor as string[];
-                    return chart.data.labels?.map((value: any, index) => ({
-                        datasetIndex: index,
-                        fillStyle: colors[index],
-                        strokeStyle: "#efefef",
-                        text: value
-                    })) || []
-                }
-            }
-        },
-        tooltip: {enabled: false},
-        datalabels: {
-            color: 'black',
-            formatter: (value, context) => value + " kWh"
-        }
-    }
+const usePieChartData: () => ChartData = () => {
+    const theme = useTheme();
+    return useMemo(() => ({
+        labels: ['Viel Strom', 'Durchschnittlicher Strom', 'Wenig Strom'],
+        datasets: [
+            {
+                data: [40, 35, 25],
+                backgroundColor: [green["800"], yellow["800"], red["800"]],
+                borderColor: theme.palette.background.paper,
+            },
+        ],
+    }), [theme]);
 }
 
-const barChartOptions: ChartOptions = {
-    scales: {
-        xAxis: {
-            ticks: {display: false},
-            grid: {display: false}
+const useBarChartOptions: () => ChartOptions = () => {
+    const theme = useTheme();
+    return useMemo(() => ({
+        responsive: true,
+        scales: {
+            xAxis: {display: false},
+            yAxis: {
+                position: "right",
+                grid: {
+                    drawBorder: false,
+                    color: theme.palette.divider
+                },
+                ticks: {
+                    callback: tickValue => tickValue + " kWh",
+                    padding: 10,
+                    stepSize: 1,
+                    color: theme.palette.text.primary,
+                    font: {family: theme.typography.fontFamily}
+                }
+            },
         },
-        yAxis: {display: false}
-    },
-    plugins: {
-        legend: {position: 'bottom'},
-        tooltip: {enabled: false},
-        datalabels: {
-            color: 'black',
-            formatter: (value, context) => value + "%"
+        plugins: {
+            legend: {
+                position: 'bottom',
+                labels: {
+                    font: {family: theme.typography.fontFamily},
+                    color: theme.palette.text.primary
+                }
+            },
+            tooltip: {enabled: false},
+            datalabels: {
+                color: theme.palette.primary.contrastText,
+                font: {
+                    family: theme.typography.fontFamily,
+                    size: 20,
+                },
+                formatter: value => value + " kWh"
+            }
         }
-    }
+    }), [theme]);
+}
+const usePieChartOptions: () => ChartOptions = () => {
+    const theme = useTheme();
+    return useMemo(() => ({
+        responsive: true,
+        scales: {
+            xAxis: {display: false},
+            yAxis: {display: false}
+        },
+        plugins: {
+            legend: {
+                position: 'bottom',
+                labels: {
+                    font: {family: theme.typography.fontFamily},
+                    color: theme.palette.text.primary
+                }
+            },
+            tooltip: {enabled: false},
+            datalabels: {
+                color: theme.palette.primary.contrastText,
+                font: {
+                    family: theme.typography.fontFamily,
+                    size: 20,
+                },
+                formatter: (value, context) => value + "%"
+            }
+        }
+    }), [theme]);
 }
 
 function Feedback(props: PrivateRouteProps) {
@@ -100,6 +127,11 @@ function Feedback(props: PrivateRouteProps) {
     const dateParsed = useParsedDate(date);
     const validDate = isValid(dateParsed) && isPast(dateParsed);
     const {setAppBar} = props;
+
+    const barChartData = useBarChartData();
+    const barChartOptions = useBarChartOptions();
+    const pieChartData = usePieChartData();
+    const pieChartOptions = usePieChartOptions();
 
     useEffect(() => {
         setAppBar({
@@ -116,17 +148,15 @@ function Feedback(props: PrivateRouteProps) {
             <Grid container spacing={1}>
                 <Grid item xs={12} md={6}>
                     <Paper variant="outlined" sx={{p: 2, height: "100%", display: "flex", alignItems: "flex-end"}}>
-                        <Bar plugins={[ChartDataLabels]} data={barChartData} options={options} height={300}/>
+                        <Bar plugins={[ChartDataLabels]} data={barChartData} options={barChartOptions} height={300}/>
                     </Paper>
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <Paper variant="outlined" sx={{p: 2, height: "100%", display: "flex", alignItems: "flex-end"}}>
-                        <Pie plugins={[ChartDataLabels]} data={pieChartData} options={barChartOptions}/>
+                        <Pie plugins={[ChartDataLabels]} data={pieChartData} options={pieChartOptions}/>
                     </Paper>
                 </Grid>
             </Grid>
-
-
         </Container>
         <InfoDialog title={t('info')} content={<Lorem/>} {...infoProps} />
     </Track>
